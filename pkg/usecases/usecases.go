39a9a15bc18e38cbb1aa81507ec0cc6d1db17e3a
Michał Czerwiński <misza@MacBookPro.lan>
2019-04-28 20:49:06 +0200

    Logging with logrus

--- pkg/usecases/usecases.go
+++ pkg/usecases/usecases.go
@@ -1,16 +1,22 @@
 package usecases
 
-import "m15.io/alpha/pkg/domain"
+import (
+	"m15.io/alpha/pkg/domain"
+
+	log "github.com/sirupsen/logrus"
+)
 
 type TaskInteractor struct {
 	TaskRepository domain.TaskRepository
 }
 
 func (interactor *TaskInteractor) CreateAndPublish(mac, sysid string) error {
+	log.Debug("Creating and publishing task")
 	task := domain.Task{
 		Mac:   mac,
 		Sysid: sysid,
 	}
+
 	err := interactor.TaskRepository.Publish(task)
 	if err != nil {
 		return err
