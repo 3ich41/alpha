7a7b36c5eb770aa49487b8731dc865d73be88ad2
Michał Czerwiński <misza@MacBookPro.lan>
2019-04-29 23:26:37 +0200

    Obsługa logowanie w przypadku złych danych

--- pkg/interfaces/webservice.go
+++ pkg/interfaces/webservice.go
@@ -1,10 +1,17 @@
 package interfaces
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+)
 
 type Task struct {
-	Mac   string `json:"mac" binding:"required"`
-	SysID string `json:"sysid" binding:"required"`
+	Mac       string `json:"mac" binding:"required"`
+	SysID     string `json:"sysid" binding:"required"`
+	IPAddr    string `json:"ipaddr" binding:"required"`
+	Username  string `json:"username" binding:"required"`
+	Timestamp string `json:"timestamp" binding:"required"`
 }
 
 type TaskInteractor interface {
@@ -19,11 +26,15 @@ func (handler WebserviceHandler) CreateTask(c *gin.Context) {
 	var task Task
 	err := c.BindJSON(&task)
 	if err != nil {
+		rawData, _ := ioutil.ReadAll(c.Request.Body)
+		log.WithFields(log.Fields{
+			"data": string(rawData),
+		}).Error("Could not bind json data to Task object. Check if data contains all required fields")
 		c.JSON(500, gin.H{"Status": "Nie utworzono zadania"})
 		return
 	}
 
-	if (task.Mac != "") && (task.SysID != "") {
+	if (task.Mac != "") && (task.SysID != "") && (task.IPAddr != "") && (task.Username != "") && (task.Timestamp != "") {
 		err := handler.TaskInteractor.CreateAndPublish(task.Mac, task.SysID)
 		if err != nil {
 			c.JSON(500, gin.H{"Status": "Nie utworzono zadania"})
@@ -33,6 +44,9 @@ func (handler WebserviceHandler) CreateTask(c *gin.Context) {
 		c.JSON(200, gin.H{"Status": "OK"})
 
 	} else {
+		log.WithFields(log.Fields{
+			"task": task,
+		}).Error("Could not process task. Fields are empty")
 		c.JSON(400, gin.H{"error": "Fields are empty"})
 	}
 }
